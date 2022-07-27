03fb28c16597ee94895099ba36b6890f42e29255
yagi <hiro.yagi0245@gmail.com>
2022-07-28 02:09:05 +0900

    first commit

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Greet } from '$lib/wailsjs/go/main/App'
+  let x = 30
+  let g = Greet('foo')
+</script>
+
+<h1>Welcome to SvelteKit a b</h1>
+
+{ x + 3.5 }
+
+<div>
+  {#await g}
+    loading
+  {:then value }
+    { value }
+  {/await}
+</div>
---
+++ frontend/vite.config.js
@@ -0,0 +1,9 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+};
+
+export default config;
