e394cb3955556e92be1069580d3b6d29b339229d
h8gi <hiro.yagi0245@gmail.com>
2022-07-28 14:39:36 +0900

    fix promise

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
   import { Greet } from '$lib/wailsjs/go/main/App'
-  let g = Greet('foo')
+  let name = ''
+  let g: Promise<string>|null = null
+
+  function onClick() {
+    if (name == '') {
+      g = null
+    } else {
+      g = Greet(name)
+    }
+  }
 </script>
 
-<h1>Welcome to SvelteKit a b</h1>
+<h1>Welcome to SvelteKit</h1>
+
+<div>Please enter your name below 👇</div>
+<input type="text" bind:value={name}/> <button on:click={onClick}>Great</button>
 
 <div>
-  {#await g}
-    loading
-  {:then value }
-    { value }
-  {/await}
+  {#if g}
+    {#await g}
+      loading
+    {:then value }
+      { value }
+    {/await}
+  {/if}
 </div>
